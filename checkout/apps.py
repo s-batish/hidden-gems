3f8f54a344cf6b84d8feb2054e7dc8b4ab00a7f6
s-batish <shivanibatish@btinternet.com>
2023-10-27 10:22:15 +0000

    Add checkout models to admin and create signals

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        """
+        Updates order totals automatically by calling update_total method
+        every time a line item is saved or deleted
+        """
+        import checkout.signals
