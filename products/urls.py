17231753a70b2e256a6acfbeebeecc8a0462d71e
s-batish <shivanibatish@btinternet.com>
2023-11-14 10:56:43 +0000

    Add delete review modal functionality

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from reviews.views import delete_review
 
 urlpatterns = [
     path('', views.all_products, name='products'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('<int:product_id>/delete_review/<int:review_id>', delete_review, name='delete_review'),
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('add_review/<int:product_id>/', views.add_review, name='add_review'),
-     path('<int:product_id>/delete_review/<int:review_id>', views.delete_review, name='delete_review'),
+    #  path('<int:product_id>/delete_review/<int:review_id>', views.delete_review, name='delete_review'),
 ]
