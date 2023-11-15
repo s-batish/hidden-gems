914e8154f775dde8b6c4cfcb1c19a187a3a7255d
s-batish <shivanibatish@btinternet.com>
2023-11-15 16:18:46 +0000

    Edit wishlist context to fix user bug

--- wishlist/contexts.py
+++ wishlist/contexts.py
@@ -1,13 +1,11 @@
-from django.shortcuts import (render,
-                              get_object_or_404, HttpResponseRedirect,
-                              redirect, reverse)
 from .models import Wishlist
 
 
 def wishlist_contents(request):
-    """Context to show totla number of items in wishlist"""
-    wishlist = Wishlist.objects.filter(user=request.user)
+    """Context to show total number of items in wishlist"""
+    context = {}
     if request.user.is_authenticated:
+        wishlist = Wishlist.objects.filter(user=request.user)
         wishlist_count = wishlist.count()
         context = {
             'wishlist_count': wishlist_count,
