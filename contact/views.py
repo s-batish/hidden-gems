060d1e26d8c3dcf9dc504eab08beaf433d2e2973
s-batish <shivanibatish@btinternet.com>
2023-11-16 14:54:22 +0000

    Add initial functionality for contact form

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact(request):
-    """View to show the contact us page"""
+    """View to show the contact us page and to create the functionality to send emails"""
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent.')
+            return render(request, 'home/index.html')
+
     form = ContactForm()
     context = {
         'form': form
